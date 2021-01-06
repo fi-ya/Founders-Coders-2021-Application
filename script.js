c20ee7dac9deb90aee2e6b2e2d7ed5559382a1f9
Safia Ali <safiaali@hotmail.com>
2021-01-06 00:21:52 +0000

    Update comments in script.js and style.css. Change navmenu CSS, flexbox alignment properties update, margin updated also, homepage buttons border radius changed

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ document.getElementById('togglemenu').addEventListener('click', ()=> {
 });
 
 // Welcome greeting 
-// array of greetings 
 const homepageGreetings = [ 'Welcome' ,'Bienvenidos' , 'أهلا بك' , 'स्वागत हे' , '欢迎' ]
 // set counter to 0
 var counter = 0;
-// function get greeting by ID, assign it to homepageGreetings location counter
+    // function get greeting by ID, assign it to homepageGreetings location counter
     setInterval(function() {
         document.getElementById('greeting').innerHTML = homepageGreetings[counter];
         counter++;
-//if counter is more than greetings array, set counter to 0
+        //if counter is more than greetings array, set counter to 0
         if (counter >= homepageGreetings.length){
             counter = 0;
         }
-// repeat every 1 second (in milliseconds)
+        // repeat every 1 second (in milliseconds)
         }, 1000); 
